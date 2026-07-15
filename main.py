c8404e950d544ed8b1a0b2281047b8abfa79ba9f
Tanpisit001 <s6903016410169@email.kmutnb.ac.th>
2026-07-15 11:37:27 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from src.calpakg.calculator import add, subtract
+from src.game.Galaxy import Game
+
+def run_calculator():
+    print("Haloo")
+    print(add(5, 3))
+    print(subtract(5, 3))
+
+def run_game():
+    Game().run()
+
+if __name__ == "__main__":
+    print("เลือกโปรแกรมที่จะรัน:")
+    print("1. Calculator")
+    print("2. Game")
+    choice = input("กรอกเลข (1/2): ")
+
+    if choice == "1":
+        run_calculator()
+    elif choice == "2":
+        run_game()
+    else:
+        print("เลือกไม่ถูกต้อง")1
